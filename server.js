4768eef865d3bf7a0b824fb1f24160d3a3e7ab87
Tetsuya Hishiki <hishikitetsuya@gmail.com>
2017-07-30 12:40:46 +0900

    test

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ http.createServer(function (req, res) {
     res.end('Hello, world!!!!!!!');
     
 }).listen(process.env.PORT || 8080);
+
+
+var coap        = require('coap')
+  , server      = coap.createServer()
+
+server.on('request', function(req, res) {
+  res.end('Hello ' + req.url.split('/')[1] + '\n')
+})
+
+server.listen(function() {
+  console.log('server started')
+})
+
